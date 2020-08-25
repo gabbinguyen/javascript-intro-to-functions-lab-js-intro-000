a12f45f00c879c6418a00581cddb2cd1d000c312
magical-builder-5600 <gabbinguyen@gmail.com>
2020-08-25 01:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase) {
     return "I can't hear you!"
-  } else if (string.toUpperCase) {
+  } else (string.toUpperCase) {
     return "YES INDEED!"
   }
 }
